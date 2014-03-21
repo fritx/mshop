775ebd1ddef28456bb1cd9a4b298e06fd6e8906f
fritx <uxfritz@163.com>
2014-03-22 02:06:47 +0800

    remove spawn.js

--- spawn.js
+++
@@ -1,8 +0,0 @@
-var spawn = require('child_process').spawn;
-var child = spawn(process.argv[2], process.argv.slice(3));
-
-child.stderr.pipe(process.stderr);
-child.stdout.pipe(process.stdout);
-child.on('close', function () {
-  process.exit();
-});
